8d182e4c607b93ee2c0eccc4fa702c3464420bff
Sheetal Gupta <sheetalg.me@gmail.com>
2019-12-03 08:29:48 +0530

    add modal

--- index.js
+++ index.js
@@ -34,3 +34,34 @@ $(".hov").mouseover(function(){
 $(".hov").mouseout(function(){
     $(this).removeClass("glow");
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("profile_pic");
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+$(document).keydown(function(e) {
+     if (e.key === "Escape") {
+        if(modal.style.display !== "none") {
+            modal.style.display = "none";
+        }
+    }
+});
+var esc = $.Event("keydown", { keyCode: 27 });
+modal.trigger(esc);
